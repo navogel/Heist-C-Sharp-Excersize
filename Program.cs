8e99174699eb28365ee97cd6fbb505086fe25d80
Nate V <navogel@gmail.com>
2019-11-18 09:47:20 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("plan your hist");
+            Console.WriteLine("please input tem member name");
+            string memberName = Console.ReadLine();
+             Console.WriteLine("please input tem member skill");
+            int skillLevel = Convert.ToInt32(Console.ReadLine());
+            if (skillLevel <= 0)
+            {
+                Console.WriteLine("please input tem member skill over zero");
+                skillLevel = Convert.ToInt32(Console.ReadLine());
+            }
+            double courageFactor = Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+}
